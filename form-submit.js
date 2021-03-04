02ee8c1a242c03bba60a96353748a4213577f967
sebosg <63050857+sebnemgormus@users.noreply.github.com>
2021-03-04 16:56:21 +0300

    new update

--- form-submit.js
+++ form-submit.js
@@ -4,8 +4,6 @@ let userFormDOM = document.querySelector("#userForm")
 userFormDOM.addEventListener('submit', formHandler)
 
 
-
-
 function formHandler(event) {
     event.preventDefault()
     const COMMENT = document.querySelector("#comment")
@@ -13,9 +11,6 @@ function formHandler(event) {
     if (COMMENT.value) {
         addItem(`${COMMENT.value}`)
         COMMENT.value = ""
-
-    }else {
-        alert("yorum girilmedi")
     }
 }
 
@@ -23,9 +18,11 @@ let userListDOM = document.querySelector("#userList")
 
 const addItem = (comment) => {
     let liDOM = document.createElement("li")
-    liDOM.innerHTML = `
-    ${comment}
-    `
+    liDOM.innerHTML = `${comment}` + '<button class="btn btn-secondary" onclick="Delete(this);">Sil</button> </li>';
     liDOM.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center')
     userListDOM.append(liDOM)
+}
+
+function Delete(currentEl){
+    currentEl.parentNode.parentNode.removeChild(currentEl.parentNode);
 }
